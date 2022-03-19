4a79f4010c350e8485c63bcf0e0962df80a89585
Ron Shavit <r0nsh4v1t@gmail.com>
2022-03-20 00:29:06 +0200

    wip

--- compiler/chili_typeck/src/typeck.rs
+++ compiler/chili_typeck/src/typeck.rs
@@ -22,41 +22,41 @@ impl TypeCk for ast::Ast {
 
 impl TypeCk for ast::Import {
     fn typeck(&mut self, tycx: &mut TyCtx, workspace: &mut Workspace) -> DiagnosticResult<()> {
-        let mut ty_kind = TyKind::Module(self.module_id);
+        // let mut ty_kind = TyKind::Module(self.module_id);
 
-        if !self.import_path.is_empty() {
-            // go over the import_path, resolving the module path
-            let mut current_module_id = self.module_id;
+        // if !self.import_path.is_empty() {
+        // go over the import_path, resolving the module path
+        // let mut current_module_id = self.module_id;
 
-            for (index, symbol) in self.import_path.iter().enumerate() {
-                // let binding_info = self.find_binding_info_in_module(
-                //     current_module_id,
-                //     symbol.value.as_symbol(),
-                //     symbol.span,
-                // )?;
+        // for (index, symbol) in self.import_path.iter().enumerate() {
+        // let binding_info = self.find_binding_info_in_module(
+        //     current_module_id,
+        //     symbol.value.as_symbol(),
+        //     symbol.span,
+        // )?;
 
-                // ty_kind = binding_info.ty.clone();
+        // ty_kind = binding_info.ty.clone();
 
-                // match ty_kind {
-                //     Ty::Module(id) => current_module_id = id,
-                //     _ => {
-                //         if index < self.import_path.len() - 1 {
-                //             return Err(TypeError::type_mismatch(
-                //                 symbol.span,
-                //                 Ty::Module(Default::default()).to_string(),
-                //                 ty_kind.to_string(),
-                //             ));
-                //         }
-                //     }
-                // }
-            }
-        }
+        // match ty_kind {
+        //     Ty::Module(id) => current_module_id = id,
+        //     _ => {
+        //         if index < self.import_path.len() - 1 {
+        //             return Err(TypeError::type_mismatch(
+        //                 symbol.span,
+        //                 Ty::Module(Default::default()).to_string(),
+        //                 ty_kind.to_string(),
+        //             ));
+        //         }
+        //     }
+        // }
+        //     }
+        // }
 
-        let import_ty = workspace.get_binding_info(self.binding_info_id).unwrap().ty;
+        // let import_ty = workspace.get_binding_info(self.binding_info_id).unwrap().ty;
 
-        import_ty
-            .unify(&ty_kind, tycx, workspace)
-            .map_err(|e| map_unify_err(e, import_ty, ty_kind, self.span(), tycx));
+        // import_ty
+        //     .unify(&ty_kind, tycx, workspace)
+        //     .map_err(|e| map_unify_err(e, import_ty, ty_kind, self.span(), tycx))?;
 
         Ok(())
     }
