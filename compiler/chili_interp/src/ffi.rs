fabda6b18a7b98537344e9d92e18a739b160e4a9
Ron Shavit <r0nsh4v1t@gmail.com>
2022-05-04 16:42:04 +0300

    wip

--- compiler/chili_interp/src/ffi.rs
+++ compiler/chili_interp/src/ffi.rs
@@ -21,11 +21,12 @@ macro_rules! raw_ptr {
 
 macro_rules! ffi_type {
     ($value: expr) => {
-        &mut $value as *mut ffi_type
+        &mut $value as TypePointer
     };
 }
 
 pub type RawPointer = *mut c_void;
+pub type TypePointer = *mut ffi_type;
 
 pub(crate) struct Ffi {
     libs: UstrMap<libloading::Library>,
@@ -57,28 +58,23 @@ impl Ffi {
 
         let mut cif = ffi_cif::default();
 
-        let return_type = ffi_type!(func.ret_ty.as_ffi_type());
+        let return_type = func.ret_ty.as_ffi_type();
 
-        let mut arg_types: Vec<*mut ffi_type> = Vec::with_capacity(args.len());
+        // let mut arg_types: Vec<TypePointer> = Vec::with_capacity(args.len());
 
-        for param in func.param_tys.iter() {
-            arg_types.push(ffi_type!(param.as_ffi_type()));
-        }
+        // for param in func.param_tys.iter() {
+        //     arg_types.push(param.as_ffi_type());
+        // }
+
+        // let return_type = ffi_type!(types::sint64);
+        let mut arg_types = vec![ffi_type!(types::pointer), ffi_type!(types::sint64)];
 
         if func.variadic {
             for arg in args.iter().skip(func.param_tys.len()) {
-                arg_types.push(ffi_type!(arg.as_ffi_type()));
+                arg_types.push(arg.as_ffi_type());
             }
 
-            prep_cif_var(
-                &mut cif,
-                ABI,
-                func.param_tys.len(),
-                arg_types.len(),
-                return_type,
-                arg_types.as_mut_ptr(),
-            )
-            .unwrap();
+            prep_cif_var(&mut cif, ABI, 1, 2, return_type, arg_types.as_mut_ptr()).unwrap();
         } else {
             prep_cif(
                 &mut cif,
@@ -92,19 +88,13 @@ impl Ffi {
 
         let code_ptr = CodePtr::from_ptr(*symbol);
 
-        let mut args: Vec<Argument> = args.iter().map(|a| Argument::wrap(a.clone())).collect();
         let mut call_args: Vec<RawPointer> = Vec::with_capacity(args.len());
 
-        for (index, arg) in args.iter_mut().enumerate() {
-            let ffi_type = *arg_types[index];
-            let ptr = arg
-                .inner
-                .as_ffi_arg(ffi_type.size, ffi_type.alignment.into());
+        for (arg, arg_type) in args.iter_mut().zip(arg_types) {
+            let arg_type = *arg_type;
+            let ptr = arg.as_ffi_arg(arg_type.size, arg_type.alignment.into());
             call_args.push(ptr);
         }
-
-        // let mut d: i64 = 42;
-        // call_args[2] = raw_ptr!(&mut d);
 
         let mut call_result = std::mem::MaybeUninit::<c_void>::uninit();
 
@@ -114,6 +104,8 @@ impl Ffi {
             call_result.as_mut_ptr(),
             call_args.as_mut_ptr(),
         );
+
+        let _ = arg_types;
 
         let call_result = call_result.assume_init_mut();
 
@@ -148,62 +140,54 @@ impl Ffi {
     // }
 }
 
-struct Argument {
-    inner: Value,
-}
-
-impl Argument {
-    fn wrap(value: Value) -> Self {
-        Self { inner: value }
-    }
-}
-
 trait AsFfiType {
-    unsafe fn as_ffi_type(&self) -> ffi_type;
+    unsafe fn as_ffi_type(&self) -> TypePointer;
 }
 
 impl AsFfiType for TyKind {
-    unsafe fn as_ffi_type(&self) -> ffi_type {
+    unsafe fn as_ffi_type(&self) -> TypePointer {
         match self {
-            TyKind::Bool => types::uint8,
+            TyKind::Bool => ffi_type!(types::uint8),
             TyKind::Int(ty) => match ty {
-                IntTy::I8 => types::sint8,
-                IntTy::I16 => types::sint16,
-                IntTy::I32 => types::sint32,
-                IntTy::I64 => types::sint64,
+                IntTy::I8 => ffi_type!(types::sint8),
+                IntTy::I16 => ffi_type!(types::sint16),
+                IntTy::I32 => ffi_type!(types::sint32),
+                IntTy::I64 => ffi_type!(types::sint64),
                 IntTy::Int => {
                     if IS_64BIT {
-                        types::sint64
+                        ffi_type!(types::sint64)
                     } else {
-                        types::sint32
+                        ffi_type!(types::sint32)
                     }
                 }
             },
             TyKind::Uint(ty) => match ty {
-                UintTy::U8 => types::uint8,
-                UintTy::U16 => types::uint16,
-                UintTy::U32 => types::uint32,
-                UintTy::U64 => types::uint64,
+                UintTy::U8 => ffi_type!(types::uint8),
+                UintTy::U16 => ffi_type!(types::uint16),
+                UintTy::U32 => ffi_type!(types::uint32),
+                UintTy::U64 => ffi_type!(types::uint64),
                 UintTy::Uint => {
                     if IS_64BIT {
-                        types::uint64
+                        ffi_type!(types::uint64)
                     } else {
-                        types::uint32
+                        ffi_type!(types::uint32)
                     }
                 }
             },
             TyKind::Float(ty) => match ty {
-                FloatTy::F16 | FloatTy::F32 => types::float,
-                FloatTy::F64 => types::double,
+                FloatTy::F16 | FloatTy::F32 => ffi_type!(types::float),
+                FloatTy::F64 => ffi_type!(types::double),
                 FloatTy::Float => {
                     if IS_64BIT {
-                        types::double
+                        ffi_type!(types::double)
                     } else {
-                        types::float
+                        ffi_type!(types::float)
                     }
                 }
             },
-            TyKind::Unit | TyKind::Pointer(_, _) | TyKind::MultiPointer(_, _) => types::pointer,
+            TyKind::Unit | TyKind::Pointer(_, _) | TyKind::MultiPointer(_, _) => {
+                ffi_type!(types::pointer)
+            }
             TyKind::Fn(_) => todo!(),
             TyKind::Array(_, _) => todo!(),
             TyKind::Slice(_, _) => todo!(),
@@ -212,63 +196,64 @@ impl AsFfiType for TyKind {
                 let size = st.size_of(WORD_SIZE);
                 let align = st.align_of(WORD_SIZE);
 
-                // TODO: Leak
-                let mut elements: Vec<*mut ffi_type> = vec![];
+                let mut elements: Vec<TypePointer> = vec![];
                 for field in st.fields.iter() {
-                    elements.push(ffi_type!(field.ty.as_ffi_type()));
+                    elements.push(field.ty.as_ffi_type());
                 }
                 let elements_ptr = elements.as_mut_ptr();
+
+                // TODO: Leak
                 std::mem::forget(elements);
 
-                ffi_type {
+                ffi_type!(ffi_type {
                     size,
                     alignment: align as u16,
                     type_: FFI_TYPE_STRUCT as u16,
                     elements: elements_ptr,
-                }
+                })
             }
             TyKind::Infer(_, ty) => match ty {
-                InferTy::AnyInt => types::sint64,
-                InferTy::AnyFloat => types::float,
+                InferTy::AnyInt => ffi_type!(types::sint64),
+                InferTy::AnyFloat => ffi_type!(types::float),
                 InferTy::PartialStruct(_) => todo!(),
                 InferTy::PartialTuple(_) => todo!(),
             },
-            TyKind::Never => types::void,
+            TyKind::Never => ffi_type!(types::void),
             _ => panic!("invalid type {}", self),
         }
     }
 }
 
 impl AsFfiType for Value {
-    unsafe fn as_ffi_type(&self) -> ffi_type {
+    unsafe fn as_ffi_type(&self) -> TypePointer {
         match self {
-            Value::I8(_) => types::sint8,
-            Value::I16(_) => types::sint16,
-            Value::I32(_) => types::sint32,
-            Value::I64(_) => types::sint64,
+            Value::I8(_) => ffi_type!(types::sint8),
+            Value::I16(_) => ffi_type!(types::sint16),
+            Value::I32(_) => ffi_type!(types::sint32),
+            Value::I64(_) => ffi_type!(types::sint64),
             Value::Int(_) => {
                 if IS_64BIT {
-                    types::sint64
+                    ffi_type!(types::sint64)
                 } else {
-                    types::sint32
+                    ffi_type!(types::sint32)
                 }
             }
-            Value::U8(_) => types::uint8,
-            Value::U16(_) => types::uint16,
-            Value::U32(_) => types::uint32,
-            Value::U64(_) => types::uint64,
+            Value::U8(_) => ffi_type!(types::uint8),
+            Value::U16(_) => ffi_type!(types::uint16),
+            Value::U32(_) => ffi_type!(types::uint32),
+            Value::U64(_) => ffi_type!(types::uint64),
             Value::Uint(_) => {
                 if IS_64BIT {
-                    types::uint64
+                    ffi_type!(types::uint64)
                 } else {
-                    types::uint32
+                    ffi_type!(types::uint32)
                 }
             }
-            Value::F32(_) => types::float,
-            Value::F64(_) => types::double,
-            Value::Bool(_) => types::uint8,
+            Value::F32(_) => ffi_type!(types::float),
+            Value::F64(_) => ffi_type!(types::double),
+            Value::Bool(_) => ffi_type!(types::uint8),
             Value::Aggregate(_) => todo!(),
-            Value::Pointer(..) => types::pointer,
+            Value::Pointer(..) => ffi_type!(types::pointer),
             Value::Func(_) => todo!(),
             _ => panic!("can't pass `{}` through ffi", self.to_string()),
         }
@@ -332,9 +317,9 @@ impl AsFfiArg for Value {
                     }
 
                     // TODO: this could be more efficient
-                    let value_size = value.as_ffi_type().size;
+                    let value_size = (*value.as_ffi_type()).size;
                     if value_size < alignment {
-                        let padding = alignment - value.as_ffi_type().size;
+                        let padding = alignment - value_size;
                         bytes.put_bytes(0, padding);
                     }
                 }
@@ -342,8 +327,8 @@ impl AsFfiArg for Value {
                 bytes.as_mut_ptr() as RawPointer
             }
             Value::Pointer(ptr) => {
-                // raw_ptr!(ptr.as_raw())
-                raw_ptr!(ptr.as_inner_raw())
+                raw_ptr!(ptr.as_raw())
+                // raw_ptr!(ptr.as_inner_raw())
                 // raw_ptr!("hallo\n\0".as_ptr() as RawPointer)
             }
             Value::Func(_) => todo!("func"),
