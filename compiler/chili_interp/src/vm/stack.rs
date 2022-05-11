c55817cf1f952fe86dc80641b53d96b550902c8c
Ron Shavit <r0nsh4v1t@gmail.com>
2022-05-12 00:21:22 +0300

    wip

--- compiler/chili_interp/src/vm/stack.rs
+++ compiler/chili_interp/src/vm/stack.rs
@@ -1,81 +1,82 @@
-pub struct Stack<T, const CAPACITY: usize>(Vec<T>);
+pub struct Stack<T, const CAPACITY: usize> {
+    inner: Vec<T>,
+}
 
-impl<T, const CAPACITY: usize> Stack<T, CAPACITY>
-where
-    T: ToString,
-{
+impl<T: ToString, const CAPACITY: usize> Stack<T, CAPACITY> {
     pub fn new() -> Self {
-        Self(Vec::with_capacity(CAPACITY))
+        Self {
+            inner: Vec::with_capacity(CAPACITY),
+        }
     }
 
     pub fn push(&mut self, value: T) {
-        assert!(self.0.len() <= self.0.capacity(), "stack overflow");
-        self.0.push(value);
+        assert!(self.inner.len() <= self.inner.capacity(), "stack overflow");
+        self.inner.push(value);
     }
 
     #[inline]
     pub fn pop(&mut self) -> T {
-        self.0.pop().unwrap()
+        self.inner.pop().unwrap()
     }
 
     #[inline]
     pub fn peek(&self, offset: usize) -> &T {
-        &self.0[self.len() - 1 - offset]
+        &self.inner[self.len() - 1 - offset]
     }
 
     #[inline]
     pub fn peek_mut(&mut self, offset: usize) -> &mut T {
         let len = self.len();
-        &mut self.0[len - 1 - offset]
+        &mut self.inner[len - 1 - offset]
     }
 
     #[inline]
     pub fn last(&self) -> &T {
-        self.0.last().unwrap()
+        self.inner.last().unwrap()
     }
 
     #[inline]
     pub fn last_mut(&mut self) -> &mut T {
-        self.0.last_mut().unwrap()
+        self.inner.last_mut().unwrap()
     }
 
     #[inline]
     pub fn take(&mut self, offset: usize) -> T {
-        self.0.remove(self.len() - 1 - offset)
+        self.inner.remove(self.len() - 1 - offset)
     }
 
     #[inline]
     pub fn get(&self, index: usize) -> &T {
-        &self.0[index]
+        &self.inner[index]
     }
 
     #[inline]
     pub fn get_mut(&mut self, index: usize) -> &mut T {
-        &mut self.0[index]
+        &mut self.inner[index]
     }
 
     #[inline]
     pub fn set(&mut self, index: usize, value: T) {
-        self.0[index] = value;
+        self.inner[index] = value;
     }
 
     #[inline]
     pub fn truncate(&mut self, len: usize) {
-        self.0.truncate(len)
+        self.inner.truncate(len)
     }
 
     #[inline]
     pub fn len(&self) -> usize {
-        self.0.len()
+        self.inner.len()
     }
 
     #[inline]
     pub fn is_empty(&self) -> bool {
-        self.0.is_empty()
+        self.inner.is_empty()
     }
 
     #[inline]
     pub fn iter(&self) -> std::slice::Iter<T> {
-        self.0.iter()
+        self.inner.iter()
     }
 }
