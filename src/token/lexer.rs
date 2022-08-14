f8beea00beb40582efaa517e1b428c41df7c7496
Ron Shavit <r0nsh4v1t@gmail.com>
2022-08-14 11:44:26 +0300

    Remove redundant code

--- src/token/lexer.rs
+++ src/token/lexer.rs
@@ -64,21 +64,6 @@ impl<'lx> Lexer<'lx> {
                     // This is a comment, eat the rest of the line and then eat the next token.
                     self.eat_line();
                     self.eat_token()?
-                    // if self.eat('!') {
-                    //     if self.cursor.end_index() == 2 {
-                    //         // Explicit support for #!
-                    //         self.eat_line();
-                    //         self.eat_token()?
-                    //     } else {
-                    //         return Err(Diagnostic::error()
-                    //             .with_message("shebang is only supported on the first line of the file")
-                    //             .with_label(Label::primary(self.cursor.span(), "invalid shebang")));
-                    //     }
-                    // } else {
-                    //     return Err(Diagnostic::error()
-                    //         .with_message(format!("unknown character `{}`", ch))
-                    //         .with_label(Label::primary(self.cursor.span(), "unknown character")));
-                    // }
                 }
                 '@' => At,
                 ';' => Semicolon,
@@ -114,11 +99,6 @@ impl<'lx> Lexer<'lx> {
                 }
                 '?' => QuestionMark,
                 '/' => {
-                    // if self.eat('/') {
-                    //     // This is a comment, eat the rest of the line and then eat the next token.
-                    //     self.eat_line();
-                    //     self.eat_token()?
-                    // } else
                     if self.eat('=') {
                         FwSlashEq
                     } else {
