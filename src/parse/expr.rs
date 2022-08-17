ae164c391fb1cacd75384050bd550e30d2d27005
Ron Shavit <r0nsh4v1t@gmail.com>
2022-08-17 12:54:33 +0300

    fixed parse named struct literal

--- src/parse/expr.rs
+++ src/parse/expr.rs
@@ -168,61 +168,33 @@ impl Parser {
 
         let mut statements = vec![];
 
-        while !eat!(self, CloseCurly) && !self.eof() {
-            self.skip_newlines();
-
-            let statement = self.parse_statement().unwrap_or_else(|diag| {
-                let span = self.previous_span();
-
-                self.cache.lock().diagnostics.push(diag);
-                self.skip_until_recovery_point();
-
-                ast::Ast::Error(ast::Empty { span })
-            });
-
-            statements.push(statement);
-
-            if eat!(self, Semicolon | Newline) {
+        while !self.eof() {
+            if eat!(self, CloseCurly) {
+                return Ok(ast::Block {
+                    statements,
+                    yields: true,
+                    span: start_span.to(self.previous_span()),
+                });
+            } else if eat!(self, Semicolon | Newline) {
                 continue;
-            } else if eat!(self, CloseCurly) {
-                break;
             } else {
-                let span = Parser::get_missing_delimiter_span(self.previous_span());
-                return Err(SyntaxError::expected(
-                    span,
-                    &format!("{}, got {}", "new line, ; or }", self.peek().kind.lexeme()),
-                ));
+                let statement = self.parse_statement().unwrap_or_else(|diag| {
+                    let span = self.previous_span();
+
+                    self.cache.lock().diagnostics.push(diag);
+                    self.skip_until_recovery_point();
+
+                    ast::Ast::Error(ast::Empty { span })
+                });
+
+                statements.push(statement);
             }
         }
 
-        // while !self.eof() {
-        //     if eat!(self, CloseCurly) {
-        //         return Ok(ast::Block {
-        //             statements,
-        //             yields: true,
-        //             span: start_span.to(self.previous_span()),
-        //         });
-        //     } else if eat!(self, Semicolon | Newline) {
-        //         continue;
-        //     } else {
-        //         let statement = self.parse_statement().unwrap_or_else(|diag| {
-        //             let span = self.previous_span();
-
-        //             self.cache.lock().diagnostics.push(diag);
-        //             self.skip_until_recovery_point();
-
-        //             ast::Ast::Error(ast::Empty { span })
-        //         });
-
-        //         statements.push(statement);
-        //         // statements.push(self.parse_statement()?);
-        //     }
-        // }
-
-        // self.cache
-        //     .lock()
-        //     .diagnostics
-        //     .push(SyntaxError::expected(self.span(), "}"));
+        self.cache
+            .lock()
+            .diagnostics
+            .push(SyntaxError::expected(self.span(), "}"));
 
         self.skip_until_recovery_point();
 
