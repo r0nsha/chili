69d52161d7901be43c1b3575ce685d9017cad3d4
Ron Shavit <r0nsh4v1t@gmail.com>
2022-08-19 18:21:53 +0300

    Bundle llvm for windows

--- build.rs
+++ build.rs
@@ -1,8 +1,17 @@
 use fs_extra::dir;
 
 fn main() {
-    println!("cargo:rustc-link-search=/lib/llvm-12");
-    println!("cargo:rustc-link-lib=dylib=LLVM-12");
+    #[cfg(windows)]
+    {
+        println!("cargo:rustc-link-search=./llvm/windows");
+        println!("cargo:rustc-link-lib=dylib=LLVM-C");
+    }
+
+    #[cfg(not(windows))]
+    {
+        println!("cargo:rustc-link-search=/lib/llvm-12");
+        println!("cargo:rustc-link-lib=dylib=LLVM-12");
+    }
 
     let workspace_dir = std::env::current_dir().unwrap();
 
