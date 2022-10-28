246ffedbea8fd1ad02ae64af54c0c6868b7d8066
r0nsha <r0nsh4v1t@gmail.com>
2022-10-28 15:39:01 +0300

    Change comment syntax back to //

--- src/token/lexer.rs
+++ src/token/lexer.rs
@@ -54,11 +54,6 @@ impl<'lx> Lexer<'lx> {
         } else {
             match ch {
                 '@' => At,
-                '#' => {
-                    // This is a comment, eat the rest of the line and then eat the next token.
-                    self.eat_line();
-                    self.eat_token()?
-                }
                 ';' => Semicolon,
                 ':' => Colon,
                 '(' => OpenParen,
@@ -92,7 +87,11 @@ impl<'lx> Lexer<'lx> {
                 }
                 '?' => QuestionMark,
                 '/' => {
-                    if self.eat('=') {
+                    if self.eat('/') {
+                        // This is a comment, eat the rest of the line and then eat the next token.
+                        self.eat_line();
+                        self.eat_token()?
+                    } else if self.eat('=') {
                         FwSlashEq
                     } else {
                         FwSlash
